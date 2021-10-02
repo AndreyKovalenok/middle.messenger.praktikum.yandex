e10d746ee966835c79cd84cd8e4920a7c2e09fef
kv <kv@kode.ru>
2021-10-03 00:51:36 +0200

    chore: add css modules

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,12 @@
 module.exports = {
+  modules: true,
   plugins: [
     require('autoprefixer'),
     require('postcss-nested'),
     require('precss'),
     require('postcss-sorting'),
+    require("postcss-modules")({
+      scopeBehaviour: "global", // can be 'global' or 'local',
+    }),
   ]
-}
+} 
