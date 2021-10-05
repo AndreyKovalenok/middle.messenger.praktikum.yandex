f1fdc41cc3c37ac5f3da0323028d51b1468495c3
kv <kv@kode.ru>
2021-10-05 21:30:34 +0200

    feat(app): add tmp base routes

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,9 @@
-import { LoginPage, RegisterPage, Page500 } from 'pages'
+import { 
+  LoginPage, 
+  RegisterPage, 
+  Page500,
+  Page404
+} from 'pages'
 import { partials } from 'shared/ui'
 
 import './style/style.scss'
@@ -7,5 +12,19 @@ partials.forEach(partial => partial())
 
 const root = document.getElementById("root");
 
-root.innerHTML = Page500();
+const locationPathname = window.location.pathname
+
+if (locationPathname === '/') {
+  window.location.replace('/login')
+}
+
+const baseRoutes = {
+  '/login': LoginPage,
+  '/registration': RegisterPage,
+  '/500': Page500,
+  '/400': Page404
+}
+
+const currentPage = baseRoutes[locationPathname]
    
+root.innerHTML = currentPage?.() ?? Page404();
