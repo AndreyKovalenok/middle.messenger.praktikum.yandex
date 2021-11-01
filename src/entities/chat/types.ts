801e0708f09309c3b5bfc03d6e310b63adaea31a
kv <kv@kode.ru>
2021-11-01 23:28:25 +0200

    feat(chat): add messages

--- src/entities/chat/types.ts
+++ src/entities/chat/types.ts
@@ -6,3 +6,14 @@ export type TChatItem = {
   message: string;
   unreadMessages: number;
 };
+
+export type TMessage = {
+  text: string;
+  time: string;
+  isUserMessage: boolean;
+};
+
+export type TDay = {
+  date: string;
+  messages: TMessage[];
+};
